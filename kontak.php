da4dc06b0294f742c607ef942ffecda224b9b928
sultonnastain <62373853+sultonnastain@users.noreply.github.com>
2020-04-29 23:23:42 +0700

    Add files via upload

--- kontak.php
+++ kontak.php
@@ -1,51 +1,29 @@
 <?php 
-	include"../inc/config.php"; 
-	validate_admin_not_login("login.php");
-	
-	
-	if(!empty($_GET)){
-		if($_GET['act'] == 'delete'){
+	include"inc/config.php";
+	include"layout/header.php";	
+?> 
+		 
+		<div class="col-md-9">
+			<div class="row">
+			<div class="col-md-12">
 			
-			$q = mysqli_query($koneksi,"delete from kontak WHERE id='$_GET[id]'");
-			if($q){ alert("Success"); redir("kontak.php"); }
-		}  
-	}
-	if(!empty($_GET['act']) && $_GET['act'] == 'create'){
-		if(!empty($_POST)){
+			<?php 
+				if(!empty($_POST)){
 			extract($_POST); 
 		
 			$q = mysqli_query($koneksi,"insert into kontak Values(NULL,'$nama','$email','$subjek','$pesan')");
-			if($q){ alert("Success"); redir("kontak.php"); }
-		}
-	}
-	if(!empty($_GET['act']) && $_GET['act'] == 'edit'){
-		if(!empty($_POST)){
-			extract($_POST); 
-		
-			$q = mysqli_query($koneksi,"update kontak SET nama='$nama',email='$email',subjek='$subjek',pesan='$pesan' where id=$_GET[id]") or die(mysql_error());
-			if($q){ alert("Success"); redir("kontak.php"); }
-		}
-	}
-	
-	
-	include"inc/header.php";
-	
-?> 
-	
-	<div class="container">
-		<?php
-			$q = mysqli_query($koneksi,"select*from kontak");
-			$j = mysqli_num_rows($q);
-		?>
-		<h4>Daftar Kontak Masuk (<?php echo ($j>0)?$j:0; ?>)</h4>
-		<a class="btn btn-sm btn-primary" href="kontak.php?act=create">Add Data</a>
-		<hr>
-		<?php
-			if(!empty($_GET)){
-				if($_GET['act'] == 'create'){
-				?>
-					<div class="row col-md-6">
-					<form action="" method="post" enctype="multipart/form-data">
+				if($q){  
+			?>
+
+			<div class="alert alert-success">Terimakasih atas masukannya</div>
+				<?php }else{ ?>
+			<div class="alert alert-danger">Terjadi kesalahan dalam pengisian form. Data belum terkirim.</div>
+				<?php } } ?>
+			<h3>Kontak Kami</h3>
+				<hr>
+				<div class="col-md-8 content-menu" style="margin-top:-20px;">
+				 
+				 <form action="" method="post" enctype="multipart/form-data">
 						<label>Nama</label><br>
 						<input type="text" class="form-control" name="nama" required><br>
 						<label>Email</label><br>
@@ -56,63 +34,12 @@
 						<textarea class="form-control" name="pesan" required></textarea><br>
 						<input type="submit" name="form-input" value="Simpan" class="btn btn-success">
 					</form>
-					</div>
-					<div class="row col-md-12"><hr></div>
-				<?php	
-				} 
-				if($_GET['act'] == 'edit'){
-					$data = mysqli_fetch_object(mysqli_query($koneksi,"select*from kontak where id='$_GET[id]'"));
-				?>
-					<div class="row col-md-6">
-					<form action="kontak.php?act=edit&&id=<?php echo $_GET['id'] ?>" method="post" enctype="multipart/form-data">
-						<label>Nama</label><br>
-						<input type="text" class="form-control" name="nama" value="<?php echo $data->nama; ?>" required><br>
-						<label>Email</label><br>
-						<input type="email" class="form-control" name="email" value="<?php echo $data->email; ?>" required><br>
-						<label>Subjek</label><br>
-						<input type="text" class="form-control" name="subjek" value="<?php echo $data->subjek; ?>" required><br>
-						<label>Pesan</label><br>
-						<textarea class="form-control" name="pesan" required><?php echo $data->pesan; ?></textarea><br>
-						<input type="submit" name="form-edit" value="Simpan" class="btn btn-success">
-					</form>
-					</div>
-					<div class="row col-md-12"><hr></div>
-				<?php	
-				} 
-			}
-		?>
-		
-		<table class="table table-striped table-hove"> 
-			<thead> 
-				<tr> 
-					<th>#</th> 
-					<th>Nama</th> 
-					<th>Email</th> 
-					<th>Subjek</th> 
-					<th>Pesan</th> 
-					<th>*</th> 
-				</tr> 
-			</thead> 
-			<tbody> 
+				 
+				</div>   
+				 
+					
 				
-
-			
-			
-		<?php while($data=mysqli_fetch_object($q)){ ?> 
-				<tr> 
-					<th scope="row"><?php echo $no++; ?></th> 
-					<td><?php echo $data->nama ?></td> 
-					<td><?php echo $data->email ?></td> 
-					<td><?php echo $data->subjek ?></td> 
-					<td><?php echo $data->pesan ?></td> 
-					<td>
-						<a class="btn btn-sm btn-success" href="kontak.php?act=edit&&id=<?php echo $data->id ?>">Edit</a>
-						<a class="btn btn-sm btn-danger" href="kontak.php?act=delete&&id=<?php echo $data->id ?>">Delete</a>
-					</td> 
-				</tr>
-		<?php } ?>
-			</tbody> 
-		</table> 
-    </div> <!-- /container -->
-	
-<?php include"inc/footer.php"; ?>
+			</div>
+			</div> 
+		</div> 	
+<?php include"layout/footer.php"; ?>
